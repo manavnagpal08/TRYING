6c15c9184b82ed2de1258c96902c9617665a4312
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-07-11 11:05:49 +0530

    Update screener.py

--- screener.py
+++ screener.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import re
+import os
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
